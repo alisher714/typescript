a4dd99d86225424844992155635bc18bff14c458
alisher714 <alishermalik1100@gmail.com>
2023-01-08 16:24:19 +0500

    typescript objects

---
+++ project0/index.js
@@ -0,0 +1,9 @@
+var ok = true;
+var newcar;
+newcar = {
+    name: "honda",
+    color: "red",
+    model: 2022,
+    owner: "Ali sher"
+};
+console.log(newcar);
---
+++ project0/index.ts
@@ -0,0 +1,26 @@
+
+
+type car = {
+    name: string,
+    color: string,
+    model: number,
+    owner: string
+}
+
+let newcar: car = {
+    name: "honda",
+    color: "red",
+    model: 2022,
+    owner: "Ali sher"
+}
+
+let newcar2: car = {
+    name: "toyota",
+    color: "grey",
+    model: 2023,
+    owner: "Ali Sher"
+}
+
+
+
+console.log(newcar);
